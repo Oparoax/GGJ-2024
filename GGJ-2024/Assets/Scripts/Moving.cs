4f628e1ea734d0576d4ee5beceb9c158ee97368e
DB246020 <daibak08@gmail.com>
2024-01-26 20:30:29 +0000

    Basic Networking movement setup

---
+++ GGJ-2024/Assets/Scripts/Moving.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+
+public class Moving : NetworkBehaviour
+{
+
+    public float MoveSpeed = 5f;
+
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        Move();
+        
+    }
+
+    [Client (RequireOwnership = true)]
+    private void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 offset = new Vector3(horizontal, Physics.gravity.y, vertical) * MoveSpeed * Time.deltaTime;
+
+        characterController.Move(offset);
+    }
+}
