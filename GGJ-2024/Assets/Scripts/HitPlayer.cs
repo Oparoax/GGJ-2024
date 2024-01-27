1ead37ed4a7edd0aa4a6e7bb64d307157255ef13
DB246020 <daibak08@gmail.com>
2024-01-27 19:27:54 +0000

    Physics fun with balloon swords

---
+++ GGJ-2024/Assets/Scripts/HitPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlayer : MonoBehaviour
+{
+    [SerializeField] GameObject SelfIgnore;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.gameObject != SelfIgnore)
+        {
+            Debug.Log("Hit:" + other.gameObject.name);
+            other.GetComponent<Rigidbody>().AddForce(SelfIgnore.transform.forward * 50f, ForceMode.Impulse);
+        }
+    }
+}
